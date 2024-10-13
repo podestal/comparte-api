ad6d10e997de35969deaac67064ac0b032014d53
podestal <luisrodriguezpodesta@gmail.com>
2024-10-13 14:39:41 -0400

    nginx implemented

--- app/comparte/settings/production.py
+++ app/comparte/settings/production.py
@@ -24,6 +24,11 @@ DATABASES = {
     }
 }
 
+STATIC_URL = "/static/static/"
+MEDIA_URL = "/static/media/"
+MEDIA_ROOT = "/vol/web/media"
+STATIC_ROOT = "/vol/web/static"
+
 # CORS_ALLOWED_ORIGINS = []
 # CORS_ALLOWED_ORIGINS.extend(
 #     filter(None, os.environ.get("DJANGO_CORS_ALLOWED_ORIGINS", "").split(","))
