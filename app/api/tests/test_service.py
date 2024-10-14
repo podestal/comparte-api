787c17b3207b195437fb492ce991fe0de91d9aed
podestal <luisrodriguezpodesta@gmail.com>
2024-10-14 11:53:07 -0400

    moving user fixtures to another file

--- app/api/tests/test_service.py
+++ app/api/tests/test_service.py
@@ -1,47 +1,13 @@
 import pytest
-from rest_framework.test import APIClient
 from rest_framework import status
 from model_bakery import baker
 from api.models import Service
-from core.models import User
-
-
-@pytest.fixture
-def api_client():
-    """Fixture to provide APIClient instance."""
-    return APIClient()
 
 
 @pytest.fixture
 def create_service():
     """Fixture to create a Service instance."""
     return baker.make(Service)
-
-
-@pytest.fixture
-def create_user():
-    """Fixture to create a normal user."""
-    return baker.make(User)
-
-
-@pytest.fixture
-def create_admin_user():
-    """Fixture to create a admin."""
-    return baker.make(User, is_staff=True)
-
-
-@pytest.fixture
-def authenticated_user(api_client, create_user):
-    """Fixture to authenticate a normal user."""
-    api_client.force_authenticate(user=create_user)
-    return api_client
-
-
-@pytest.fixture
-def admin_user(api_client, create_admin_user):
-    """Fixture to authenticate a admin user."""
-    api_client.force_authenticate(user=create_admin_user)
-    return api_client
 
 
 @pytest.mark.django_db
